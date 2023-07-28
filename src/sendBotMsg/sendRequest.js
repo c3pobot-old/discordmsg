0c1f240a370b88e1abf5fa7a790c32140bf637da
scuba <scuba75@gmail.com>
2023-07-28 22:23:28 +0000

    update

--- src/sendBotMsg/sendRequest.js
+++ src/sendBotMsg/sendRequest.js
@@ -4,38 +4,34 @@ const path = require('path')
 const BOT_BRIDGE_URI = process.env.BOT_BRIDGE_URI
 const parseResponse = require('./parseResponse')
 
-const postRequest = async(uri, body)=>{
+const fetchRequest = async(uri, opts = {})=>{
   try{
-    const res =  await fetch(BOT_BRIDGE_URI+'/cmd', {
-      method: 'POST',
-      body: JSON.stringify(body),
-      timeout: 120000,
-      compress: true,
-      headers: {"Content-Type": "application/json"}
-    })
+    let res = await fetch(uri, opts)
     return await parseResponse(res)
   }catch(e){
-    if(e?.name) return {error: e.name, message: e.message, type: e.type}
+    if(e?.error) return {error: e.name, message: e.message, type: e.type}
     if(e?.status) return await parseResponse(e)
-    console.error(e)
+    throw(e)
   }
 }
-
-const sendRequest = async(body, count = 0)=>{
+const requestWithRetry = async(uri, opts = {}, count = 0)=>{
   try{
-    let res = await postRequest(uri, body)
-    if(res?.error === 'FetchError' && count < 11){
+    let res = await fetchRequest(uri, opts)
+    if(res?.error === 'FetchError' && 10 >= count){
       count++
-      return await sendRequest(uri, body, count)
+      return await requestWithRetry(uri, opts, count)
     }
     return res
   }catch(e){
-    console.error(e);
+    throw(e)
   }
 }
 module.exports = async(body = {})=>{
   try{
-    return await sendRequest(body)
+    let opts = {method: 'POST', timeout: 120000, compress: true, headers: {"Content-Type": "application/json"}, body: JSON.stringify(body)}
+    let res = await requestWithRetry(path.join(BOT_BRIDGE_URI, 'cmd'), opts)
+    if(res?.body) return res.body
+    console.error(res)
   }catch(e){
     console.error(e);
   }
