61f8145fb46e014a9026443d6674c6f00f5098d8
scuba <scuba75@gmail.com>
2023-08-07 19:53:26 +0000

    update

--- src/sendBotMsg/sendRequest.js
+++ src/sendBotMsg/sendRequest.js
@@ -3,7 +3,7 @@ const fetch = require('./fetch')
 const BOT_NODE_NAME_PREFIX = process.env.BOT_NODE_NAME_PREFIX || 'bot'
 const BOT_SVC = process.env.BOT_SVC || 'bot:3000'
 
-let BOT_TOTAL_SHARDS, notify = true, retryCount = 10
+let BOT_TOTAL_SHARDS = 1, notify = true, retryCount = 10
 const getPodName = async(obj = {})=>{
   try{
     if(BOT_TOTAL_SHARDS === 1) return `${BOT_NODE_NAME_PREFIX}-0`
@@ -31,12 +31,14 @@ const requestWithRetry = async(uri, opts = {}, count = 0)=>{
     throw(e)
   }
 }
-const allBotRequest = async(opts = {})=>{
+const allBotRequest = async(cmd, opts = {})=>{
   try{
     let array = [], res = [], i = BOT_TOTAL_SHARDS
     const singleBotRequest = async(shardId, opt = {})=>{
       try{
-        let obj = await requestWithRetry(`http://${BOT_NODE_NAME_PREFIX}-${shardId}.${BOT_SVC}/cmd`, {...opt,...{ podName: `${BOT_NODE_NAME_PREFIX}-${shardId}` }})
+        let payload = { method: 'POST', timeout: 60000, compress: true, headers: {"Content-Type": "application/json"} }
+        payload.body = JSON.stringify({ ...opts, ...{ cmd: cmd, podName: `${BOT_NODE_NAME_PREFIX}-${shardId}` } })
+        let obj = await requestWithRetry(`http://${BOT_NODE_NAME_PREFIX}-${shardId}.${BOT_SVC}/cmd`, payload)
         if(obj?.body) res.push(obj.body)
       }catch(e){
         throw(e);
@@ -72,10 +74,17 @@ module.exports = async(cmd, opts = {})=>{
     let podName = opts.podName
     if(!podName) podName = await getPodName(opts)
     if(!podName) throw('Error getting podName...')
+    if(podName === 'all') return await allBotRequest(cmd, opts)
     let payload = { method: 'POST', timeout: 60000, compress: true, headers: {"Content-Type": "application/json"} }
-    payload.body = JSON.stringify({ ...opts, ...{ cmd: cmd, podName: podName } })
+    payload.body = JSON.stringify({ ...opts, ...{ cmd: cmd, podName: podName, numShards: BOT_TOTAL_SHARDS } })
     if(podName === 'all') return await allBotRequest(payload)
     let res = await requestWithRetry(`http://${podName}.${BOT_SVC}/cmd`, payload)
+    if(res?.body?.totalShards){
+      log.info(`Change in number of ${BOT_NODE_NAME_PREFIX} shards from ${BOT_TOTAL_SHARDS} to ${res.body.totalShards}`)
+      BOT_TOTAL_SHARDS = +res?.body?.totalShards
+      payload.body = JSON.stringify({ ...opts, ...{ cmd: cmd, podName: podName, numShards: BOT_TOTAL_SHARDS } })
+      res = await requestWithRetry(`http://${podName}.${BOT_SVC}/cmd`, payload)
+    }
     if(res?.body) return res.body
     throw(res)
   }catch(e){
